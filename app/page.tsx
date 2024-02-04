6e74933f0bcde1b3297762e252cc6e1a69a2a6d8
AliMoharramy <alimoharrami.dev@gmail.com>
2024-02-04 22:38:46 +0330

    beforedatabase

--- app/page.tsx
+++ app/page.tsx
@@ -29,6 +29,9 @@ export default function Page() {
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
+          <Link href="/dashboard">
+            <span>dashboard</span>
+          </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
