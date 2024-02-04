c6276a8b853935b433e182752b4ad4aa27f83ad7
AliMoharramy <alimoharrami.dev@gmail.com>
2024-02-04 17:26:00 +0330

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Lusitana, Inter } from 'next/font/google';
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+export const inter = Inter({ subsets: ['latin'] });
